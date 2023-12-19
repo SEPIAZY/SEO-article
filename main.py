111dc96b7c441aeb23e00cfe90d54e0744b4eb8a
RiwRiwara <awirut2629@gmail.com>
2023-12-19 19:32:22 +0700

    Complate Landing Page

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 from flask_cors import CORS
 
 app = Flask(__name__, static_folder='static')
 CORS(app)
 
+content = {
+    'en': {
+        'greeting': 'Hello!',
+        'message': 'Welcome to our website.'
+    },
+    'th': {
+        'greeting': 'สวัสดี!',
+        'message': 'ยินดีต้อนรับสู่เว็บไซต์ของเรา'
+    }
+}
+
 @app.route('/')
 def index():
-    return render_template('./index.html')
-# image host
+    lang = request.args.get('lang', 'en')  
+    lang_content = content.get(lang, content['en']) 
+    return render_template('index.html', lang_content=lang_content)
+
 @app.route('/img/<filename>')
 def send_img(filename):
     return send_from_directory('./static/img', filename)
