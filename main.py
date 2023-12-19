1c7600880f4c4315e573485198fb408546f6a8f9
RiwRiwara <awirut2629@gmail.com>
2023-12-19 17:21:23 +0700

    add section 2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, session
+from flask import Flask, render_template, send_from_directory
 from flask_cors import CORS
 
 app = Flask(__name__, static_folder='static')
@@ -7,7 +7,10 @@ CORS(app)
 @app.route('/')
 def index():
     return render_template('./index.html')
-
+# image host
+@app.route('/img/<filename>')
+def send_img(filename):
+    return send_from_directory('./static/img', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
